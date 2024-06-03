bb9445abc729223776dcf714c8c13cbb8e768a0c
Beata <beata@jasinski.com>
2024-06-03 12:12:53 -0700

    func factorial

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	fact := factorial(5)
+
+	fmt.Println((fact))
+}
+
+func factorial(number int) int {
+	result := 1
+
+	for i := 1; i <= number; i++ {
+		result = result * i
+	}
+
+	return result
 }
